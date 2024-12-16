e6bb39b39026ff9386dc8a9bb14e0a3ef985da5c
Angel Romero <mrangelrcd@gmail.com>
2024-12-16 19:30:27 +0100

    exercise 1.31 accumulate procedure

--- week2.rkt
+++ week2.rkt
@@ -10,6 +10,8 @@
 
 (define (inc x) (+ x 1))
 
+(define (identity n)
+  n)
 
 ;; EXERCISE 1.29
 (define (simpson f a b n)
@@ -24,4 +26,39 @@
       ((= (remainder k 2) 0) 2)
       (else 4)))
   (* (/ h 3) (sum term 0.0 inc n))
+)
+
+;; EXERCISE 1.31
+(define (product term a next b)
+  (if (> a b)
+    1
+    (* (term a) (product term (next a) next b))
+  )
+)
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(define (pi-aprox n)
+  (* 4.0 (/
+    (* 2 (product (lambda (x) (* x x)) 4 (lambda (x) (+ x 2)) n))
+    (product (lambda (x) (* x x)) 3 (lambda (x) (+ x 2)) n)
+  ))
+)
+
+;; EXERCISE 1.32
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+    null-value
+    (combiner (term a) (accumulate combiner null-value term (next a) next b)))
+)
+
+;; SUM DEFINED WITH ACCUMULATE
+(define (accumulate-sum term a next b)
+  (accumulate (lambda (x y) (+ x y)) 0 term a next b)
+)
+
+;; PRODUCT DEFINED WITH ACCUMULATE
+(define (accumulate-product term a next b)
+  (accumulate (lambda (x y) (* x y)) 1 term a next b)
 )
